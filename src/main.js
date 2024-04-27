196ca987fd83573500357e09d164230931294557
Samrityy <smritymulmi23@gmail.com>
2024-04-27 23:42:45 +0545

    feat: add router-view

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import './assets/styles.css';
+import App from './App.vue';
+import router from './router/router.js'; 
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
