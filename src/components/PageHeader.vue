0cfd188f8f121d77be3303fdd502c46572c2fa3b
Samrityy <smritymulmi23@gmail.com>
2024-04-27 23:43:05 +0545

    feat: add header component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,54 @@
+<!-- <template>
+    <nav class="bg-custom-orange">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-12">
+          <div class="flex items-center space-x-4">
+            <a href="#" class="text-white font-medium">Online order: (210)-986-2564</a>
+            <a href="#" class="text-white font-medium">Order tracking</a>
+          </div>
+          <div class="flex items-center space-x-4">
+            <p class="text-white font-medium">Free worldwide shipping for above united kingdom!</p>
+            <a href="#" class="text-white font-medium">Find our seach</a>
+            <a href="#" class="text-white font-medium">Email now: demo@demo.com</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </template> -->
+<template>
+  <nav>
+    <ul class="flex justify-between items-center text-md bg-custom-orange h-11">
+      <li class="text-white mx-auto space-x-7">
+        <a ref="#" >Online order :(210)-986- 2564</a>
+        <a ref="#" >Order Tracking</a>
+      </li>
+      <li class=" flex text-white mx-auto">
+        <p>Free worldwide shipping for above united kingdom!</p>
+      </li>
+      <li class=" flex text-white mx-auto space-x-7">
+        <a ref="#" >Find our search</a>
+        <p>Email now :demo@demo.com</p>
+      </li>
+    </ul>
+
+    <ul class="flex justify-between items-center text-lg h-24">
+      <li class="text-black mx-auto space-x-10">
+        <a ref="#" >HOME</a>
+        <a ref="#" >COLLECTION</a>
+        <a ref="#" >PRODUCT</a>
+        <a ref="#" >BLOG</a>
+        <a ref="#" >PAGES</a>
+      </li>
+      <li class=" flex text-black mx-auto">
+        <p>logo</p>
+      </li>
+      <li class=" flex text-black mx-auto space-x-7">
+        <a ref="#" >Login & Register</a>
+        <a ref="#" >My wishlist</a>
+        <a ref="#" >My Cart</a>
+
+      </li>
+    </ul>
+  </nav>
+  
+</template>
