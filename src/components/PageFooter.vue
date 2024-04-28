d47a61b38e4f8c2fd63111d96d614a149793c3a3
Samrityy <smritymulmi23@gmail.com>
2024-04-29 01:22:08 +0545

    feat: add pagefooter

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="flex justify-between item-center mx-12 my-20">
+    <div class="flex flex-col item-center w-1/3 p-2 mt-1">
+      <img class="w-25 h-14 mr-auto" src="..\assets\logo2.png" alt="logo" />
+      <p class="p-4 text-custom-grey ">
+        The middle of text all the lorem Ipsum generaton the Internet tend to
+        repeat are going to use a passage.
+      </p>
+      <span class="p-4 text-lg">Download app:</span>
+      <span class="space-x-5 p-4">
+        <button
+          class="bg-custom-orange hover:bg-blue-700 text-white font-bold py-2 px-4 w-32 h-16 rounded-lg"
+        >
+          Get it on Google Play
+        </button>
+        <button
+          class="bg-custom-orange hover:bg-blue-700 text-white font-bold py-2 px-4 w-32 h-16 rounded-lg"
+        >
+          Get it on Google Play
+        </button>
+      </span>
+    </div>
+    <div class="flex space-x-9 justify-between item-center w-1/3 ">
+      <div class="footer-section  my-4 w-1/3">
+        <div class="section-title text-lg font-semibold mb-6">Know us</div>
+        <div class="section-links text-custom-grey flex flex-col space-y-4 ">
+          <a href="#" class="hover:text-blue-500">About us</a>
+          <a href="#" class="hover:text-blue-500">Faqs</a>
+          <a href="#" class="hover:text-blue-500">Blog post</a>
+          <a href="#" class="hover:text-blue-500">Contact us</a>
+          <a href="#" class="hover:text-blue-500">My wishlist</a>
+          <a href="#" class="hover:text-blue-500">Return policy</a>
+        </div>
+      </div>
+
+      <div class="footer-section  m-4 w-1/3  ">
+        <div class="section-title text-lg font-semibold mb-6">Our store</div>
+        <div class="section-links text-custom-grey flex flex-col space-y-4">
+          <a href="#" class="hover:text-blue-500">Store map</a>
+          <a href="#" class="hover:text-blue-500">Our gallery</a>
+          <a href="#" class="hover:text-blue-500">Portfolio</a>
+          <a href="#" class="hover:text-blue-500">Main shop</a>
+          <a href="#" class="hover:text-blue-500">Store site</a>
+          <a href="#" class="hover:text-blue-500">Discount</a>
+        </div>
+      </div>
+
+      <div class="footer-section  m-4 w-1/3">
+        <div class="section-title text-lg font-semibold mb-6">Useful links</div>
+        <div class="section-links text-custom-grey flex flex-col space-y-4">
+          <a href="#" class="hover:text-blue-500">Privacy policy</a>
+          <a href="#" class="hover:text-blue-500">Return policy</a>
+          <a href="#" class="hover:text-blue-500">Faqs</a>
+          <a href="#" class="hover:text-blue-500">Payment policy</a>
+          <a href="#" class="hover:text-blue-500">Contact us</a>
+          <a href="#" class="hover:text-blue-500">Terms & condition</a>
+        </div>
+      </div>
+    </div>
+    <div class="w-1/3 my-4 mx-4 px-10 ">
+        <div class="text-lg mb-8 font-semibold">Follow on instagram</div>
+      <div class="flex  justify-between mb-8  ">
+        <img class="w-24 h-20 rounded-xl" src="..\assets\img_1.png" alt="logo" />
+        <img class="w-24 h-20 rounded-xl" src="..\assets\img_1.png" alt="logo" />
+        <img class="w-24 h-20 rounded-xl" src="..\assets\img_1.png" alt="logo" />
+      </div>
+      <div class="flex justify-between " >
+        <img class="w-24 h-20 rounded-xl" src="..\assets\img_1.png" alt="logo" />
+        <img class="w-24 h-20 rounded-xl" src="..\assets\img_1.png" alt="logo" />
+        <img class="w-24 h-20 rounded-xl" src="..\assets\img_1.png" alt="logo" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
