3aa0c3dda1ba31fdfdd26be86f1f90e1c583134d
Samrityy <smritymulmi23@gmail.com>
2024-04-28 22:23:43 +0545

    feat: add bg image and button in dashboard

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,5 +1,16 @@
 <template>
     <PageHeader/>
+    <div class="container w-full h-full mx-auto ">
+        <img src="..\\assets\\bg.jpg" alt="image" class="w-full h-full object-cover">
+    <div class="absolute top-28 right-8 bottom-2 left-6 flex flex-col m-20 w-1/2 ">
+      <h1 class="text-6xl font-bold text-black mb-6 w-1/2">Every dog has its every day</h1>
+      <p class="text-lg text-black mb-4 w-1/2">We've all heard people who say that pets are animals,as though somehow they're in our lives people have likely never owen a loyan dog !</p>
+      <p class="text-xl my-5">Starting rate:$70.00</p>
+      <button class="bg-custom-orange hover:bg-blue-700 text-white font-bold py-2 px-4 w-48 h-16  rounded-lg">
+        SHOP ONLINE 
+      </button>
+      </div>
+      </div>
 </template>
 <script setup>
 import PageHeader from '@/components/PageHeader.vue';
