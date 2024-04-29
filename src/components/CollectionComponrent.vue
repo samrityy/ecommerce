50b4a72d3c9547e8ee02f7316be07407cad08254
Samrityy <smritymulmi23@gmail.com>
2024-04-29 18:13:16 +0545

    feat: add collection dropdown

---
+++ src/components/CollectionComponrent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container bg-white flex justify-between m-16 p-20">
+        <div>
+            <ul class="space-y-5">
+                <li class="font-bold">Golden retriever</li>
+                <li> Chickpea 150g</li>
+                <li> Fresh almonds</li>
+                <li> Natural coriander</li>
+                <li> Pure mug dal</li>
+                <li> Saffron spice still</li>
+                <li> Healthy walnut</li>
+            </ul>
+        </div>
+        <div>
+            <ul class="space-y-5">
+                <li class="font-bold"> Siamese cats</li>
+                <li> Healthy walnut</li>
+                <li> Juglans walnuts</li>
+                <li> Pure mug dal</li>
+                <li> Chickpea 150g</li>
+                <li> Green moong dal</li>
+                <li> Natural coriander</li>
+            </ul>
+        </div>
+        <div>
+            <img class="rounded-xl" src="..\assets\img_1.png" alt="logo" />
+        </div>
+        <div>
+            <img class="rounded-xl" src="..\assets\img_1.png" alt="logo" />
+        </div>
+    </div>
+</template>
--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,20 +1,33 @@
 <template>
-    <PageHeader/>
-    <div class="container w-full h-full mx-auto ">
-        <img src="..\\assets\\bg.jpg" alt="image" class="w-full h-full object-cover">
-    <div class="absolute top-28 right-8 bottom-2 left-6 flex flex-col m-20 w-1/2 ">
-      <h1 class="text-6xl font-bold text-black mb-6 w-1/2">Every dog has its every day</h1>
-      <p class="text-lg text-black mb-4 w-1/2">We've all heard people who say that pets are animals,as though somehow they're in our lives people have likely never owen a loyan dog !</p>
-      <p class="text-xl my-5 font-bold">Starting rate:
-        <span class="text-custom-orange">$70.00</span></p>
-      <button class="bg-custom-orange hover:bg-blue-700 text-white font-bold py-2 px-4 w-48 h-16  rounded-lg">
-        SHOP ONLINE 
-      </button>
+  <PageHeader />
+  <div class="bg-cover" style="background-image: url('bg1.jpg'); height: 800px">
+    <div class="container w-full h-full mx-auto">
+      <div class="flex flex-col w-1/2">
+        <h1 class="text-6xl font-bold text-black mb-6 w-1/2 pt-24 mt-12">
+          Every dog has its every day
+        </h1>
+        <p class="text-lg text-black mb-4 w-1/2">
+          We've all heard people who say that pets are animals,as though somehow
+          they're in our lives people have likely never owen a loyan dog !
+        </p>
+        <p class="text-xl my-5 font-bold">
+          Starting rate: <span class="text-custom-orange">$70.00</span>
+        </p>
+        <button
+          class="bg-custom-orange hover:bg-blue-700 text-white font-bold py-2 px-4 w-48 h-16 rounded-lg transition-colors"
+        >
+          SHOP ONLINE
+        </button>
       </div>
-      </div>
-      <PageFooter/>
+    </div>
+    <div class="container w-full h-full mx-auto">
+      <PageFooter />
+    </div>
+  </div>
+  <CollectionComponrent />
 </template>
 <script setup>
-import PageHeader from '@/components/PageHeader.vue';
-import PageFooter from '@/components/PageFooter.vue';
+import PageHeader from "@/components/PageHeader.vue";
+import PageFooter from "@/components/PageFooter.vue";
+import CollectionComponrent from "@/components/CollectionComponrent.vue";
 </script>
