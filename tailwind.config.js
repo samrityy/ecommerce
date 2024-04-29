e83bcf76eb19b79bd60234aee669a8f87b16f37a
Samrityy <smritymulmi23@gmail.com>
2024-04-29 18:13:59 +0545

    feat: add vue-carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
     extend: {
       colors: {
-        'custom-orange': '#f3915c',
-        'custom-grey':'#a3a29e',
+        "custom-orange": "#f3915c",
+        "custom-grey": "#a3a29e",
       },
     },
   },
   plugins: [],
-}
-
+};
